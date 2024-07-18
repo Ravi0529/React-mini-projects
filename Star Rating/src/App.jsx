db49567099311d68fce6d8a043fe5dc211fa4fb0
Ravi0529 <150603739+Ravi0529@users.noreply.github.com>
2024-07-18 12:03:30 +0530

    mini-project

---
+++ Star Rating/src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import StarRating from './components/StarRating'
+
+function App() {
+
+  return (
+    <>
+      <StarRating noOfStars={5} />
+    </>
+  )
+}
+
+export default App
